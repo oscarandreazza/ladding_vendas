72bed5e31b11c8cc6008d7e8e973e0193b353f4e
oscarandreazza <oscarbrum09@gmail.com>
2021-09-02 23:39:17 -0300

    firstLadding

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import constructing from "./assets/waze-construction.gif";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Página em construção</h1>
+        <div>
+          <img src={constructing} alt="construct-english" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
